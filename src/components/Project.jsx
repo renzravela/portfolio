4f051d758dd2cdb0736a01fdda86f52096c90cfc
renz_aerol_ravela <renzaerol.ravela@gmail.com>
2024-08-10 19:27:13 +0800

    project commit

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -5,7 +5,7 @@ const Project = ({title, description, bg, url, title_btn, small_text}) => {
       <Card className="project_card" style={{backgroundImage: `url(${bg})`}}>
         <Card.Body className="project_content">
           <Card.Title className="card_title">{title}</Card.Title>
-          <Card.Text className="card_body">{description} <small>({small_text})</small></Card.Text>
+          <Card.Text className="card_body">{description} <small>{small_text}</small></Card.Text>
           <Button href={url} target="blank" className="project_btn" title={title_btn}>See more</Button>
         </Card.Body>
       </Card>
