bfee9abad59eb4f292ce1d4e477f987d3ebac0ec
renz_aerol_ravela <renzaerol.ravela@gmail.com>
2024-09-17 23:38:14 +0800

    commit

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -6,6 +6,7 @@ import landing_page from "./Project_Works/bgs/landing.png";
 import movie_website from "./Project_Works/bgs/Movie.png";
 import gogoanime from "./Project_Works/bgs/GogoAnime.png";
 import form from "./Project_Works/bgs/form-firebase.jpg";
+import vuejobs from "./Project_Works/bgs/vuejobs.png";
 
 const Projects = () => {
   return (
@@ -21,18 +22,24 @@ const Projects = () => {
             </p>
             <Row className="project_list">
               <Project
+                title={`Sample Landing Page UI`}
+                description={`Sample landing page UI built with react and tailwind.`}
+                bg={landing_page}
+                url={`https://renzravela.github.io/landing-page-sample/`}
+              />
+              <Project
+                title={`VueJobs Frontend`}
+                description={`Single Page Application (Frontend as of now) built with vue, typescript and tailwind.`}
+                bg={vuejobs}
+                url={`https://findyourvuejobs.netlify.app/`}
+              />
+              <Project
                 title={`Guidance Management System`}
                 description={`Full-stack web management system for the school's guidance request and reporting functions. Built mainly by Laravel.`}
                 bg={guidance}
                 url={`#`}
                 title_btn={`This work is a freelance project and I like to keep it private. Contact me for more info about this project.`}
                 small_text={`This work is a freelance project and I like to keep it private. Contact me for more info about this project.`}
-              />
-              <Project
-                title={`Sample Landing Page UI`}
-                description={`Sample landing page UI built with react and tailwind.`}
-                bg={landing_page}
-                url={`https://renzravela.github.io/landing-page-sample/`}
               />
               <Project
                 title={`Movie Review System`}
@@ -63,4 +70,3 @@ const Projects = () => {
 };
 
 export default Projects;
-
